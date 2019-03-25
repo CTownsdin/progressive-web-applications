126e47533cd33b8de2f227d28becf6c241f74324
Christian Townsdin <ctownsdin@expedia.org>
2019-03-25 08:35:44 -0700

    .

--- pwa-app/src/js/main.js
+++ pwa-app/src/js/main.js
@@ -1,13 +1,23 @@
 
+// Fetch an image and readResponseAsBlob() and append to the DOM.
+
 function validateResponse(response) {
-  if (!response.ok) {
-    throw Error(response.statusText);
+  if (!response.ok) { // is response !(2xx) ?
+    throw Error(`${response.status} ${response.statusText}`);
   }
   return response;
 }
 
 function logError(error) {
   console.log('There was an error: \n', error);
+  displayErrors(error);
+}
+
+function displayErrors(error){
+  var p = document.createElement('p');
+  p.appendChild(document.createTextNode(`Error: ${error.message}`));
+  var container = document.getElementById('image-container');  
+  document.body.insertBefore(p, container);
 }
 
 function readResponseAsBlob(response) {
@@ -31,3 +41,24 @@ function fetchImage(pathToResource) {
 }
 
 fetchImage('../img/spacex-bfr.jpg');
+
+// Make a HEAD request to check the size of a resource.
+
+function checkSize(response) {
+  var size = response.headers.get('content-length');
+  // Do stuff based on response size
+  console.log(`found size is: ${size} bytes`);
+}
+
+function headRequest(pathToResource) {
+  fetch(pathToResource, {
+    method: 'HEAD'
+  })
+  .then(validateResponse)
+  .then(checkSize)
+  // ...
+  .catch(logError);
+}
+
+headRequest('examples/words.txt');
+
